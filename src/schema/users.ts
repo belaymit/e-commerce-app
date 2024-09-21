44334504c198eb248fd96e15c6e41dd3fc49fcc9
belaymit <2belamit@gmail.com>
2024-09-21 11:46:00 +0300

    Added product validation

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -5,3 +5,10 @@ export const SignUpSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 })
+
+export const ProductSchema = z.object({
+  name: z.string().min(3),
+  price: z.number().positive(),
+  description: z.string().min(10),
+  tags: z.array(z.string())
+});
