adc898278f76b0cf59c13ec27e5467c5650699ab
belaymit <2belamit@gmail.com>
2024-09-21 11:45:19 +0300

    Added routes for product controllers

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { errorHandler } from "../error-handler";
-import { createProduct } from "../controllers/products";
+import { createProduct, deleteProduct, getProduct, getProducts, updateProduct } from "../controllers/products";
 import authMiddleware from "../middlewares/auth";
 import adminMiddleware from "../middlewares/admin";
 
 const productsRoutes: Router = Router();
 
 productsRoutes.post('/',[authMiddleware, adminMiddleware], errorHandler(createProduct));
+productsRoutes.put('/:id', [authMiddleware, adminMiddleware], errorHandler(updateProduct));
+productsRoutes.delete('/:id', [authMiddleware, adminMiddleware], errorHandler(deleteProduct));
+productsRoutes.get('/', [authMiddleware], errorHandler(getProducts));
+productsRoutes.get('/:id', [authMiddleware], errorHandler(getProduct));
 export default productsRoutes;
