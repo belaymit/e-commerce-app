0fb1b2568b8b44cb87de69adb476c9b99f682239
belaymit <2belamit@gmail.com>
2024-09-21 10:37:19 +0300

    Added products route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./auth";
+import productsRoutes from "./products";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRouter);
+rootRouter.use("/products", productsRoutes);
 
 export default rootRouter;
