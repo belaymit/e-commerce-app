1641b3994f12b8629bb4d0a0f1bfca89279a9bf0
belaymit <2belamit@gmail.com>
2024-09-22 12:49:44 +0300

    Updated arguments

--- src/exceptions/bad-request.ts
+++ src/exceptions/bad-request.ts
@@ -1,7 +1,7 @@
 import { ErrorCode, HttpException } from "./root";
 
 export class BadRequestException extends HttpException {
-  constructor(message: string, errorCode:ErrorCode) {
+  constructor(message: string, errorCode:ErrorCode, error?: Error) {
     super(message, errorCode, 400, null);
   }
 }
