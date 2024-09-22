13e21219e49a413ba5b63b016469e24ae4f596f1
belaymit <2belamit@gmail.com>
2024-09-22 23:09:25 +0300

    Added routes for user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth";
-import adminMiddleware from "../middlewares/admin";
-import { addAddress, deleteAddress, listAddresses } from "../controllers/users";
+import { addAddress, deleteAddress, listAddresses, updateAddress } from "../controllers/users";
 import { errorHandler } from "../error-handler";
 
 const usersRouter: Router = Router();
-usersRouter.post("/address", [authMiddleware, adminMiddleware], errorHandler(addAddress));
-usersRouter.delete("/address/:id", [authMiddleware, adminMiddleware], errorHandler(deleteAddress));
-usersRouter.get("/address", [authMiddleware, adminMiddleware], errorHandler(listAddresses));
+usersRouter.post("/address", [authMiddleware], errorHandler(addAddress));
+usersRouter.delete("/address/:id", [authMiddleware], errorHandler(deleteAddress));
+usersRouter.get("/address", [authMiddleware], errorHandler(listAddresses));
+usersRouter.put("/", [authMiddleware], errorHandler(updateAddress));
 
 export default usersRouter;
