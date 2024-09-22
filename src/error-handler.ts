022796ca27e9cf299dbb529c707a0e863f5f0d80
belaymit <2belamit@gmail.com>
2024-09-22 12:48:47 +0300

    Updated error handler to handle validation error

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express"
 import { ErrorCode, HttpException } from "./exceptions/root"
 import { InternalServerErrorException } from "./exceptions/internal-exceptions"
+import { ZodError } from "zod"
+import { BadRequestException } from "./exceptions/bad-request"
 
 export const errorHandler = (method: Function) => {
   return async(req: Request, res: Response, next: NextFunction) => {
@@ -11,7 +13,12 @@ export const errorHandler = (method: Function) => {
       if(error instanceof HttpException){
         exception = error;
       }else{
-        exception = new InternalServerErrorException("Something went wrong", error, ErrorCode.INTERNAL_SERVER_ERROR);
+        if(error instanceof ZodError) {
+          exception = new BadRequestException("Unprocessable Entity", ErrorCode.UNPROCESSABLE_ENTITY, error);
+        }else {
+          exception = new InternalServerErrorException("Something went wrong", error, ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+          
       }
       next(exception);
     }
