ea77168b68c8c269dadc869e8c1027b5e6ed5162
belaymit <2belamit@gmail.com>
2024-09-23 19:26:08 +0300

    Added carts controller

--- src/controllers/cart.ts
+++ src/controllers/cart.ts
@@ -33,6 +33,54 @@ export const addItemToCart = async (req: Request, res: Response) => {
   res.json(cart);
 }
 
-export const deleteItemFromCart = async (req: Request, res: Response) => {}
-export const changeItemQuantity = async (req: Request, res: Response) => {}
-export const getCart = async (req: Request, res: Response) => {}
+export const deleteItemFromCart = async (req: Request, res: Response) => {
+  //check if user is deleting his own cart item
+  const userReq = req as unknown as UserParams;
+  const cartItem = await prismaClient.cartItem.findFirst({
+    where: {
+      id: parseInt(req.params.id),
+      userId: userReq.user.id
+    }
+  })
+  if(!cartItem){
+    throw new NotFoundException("Cart item not found", ErrorCode.CART_ITEM_NOT_FOUND);
+  }
+  await prismaClient.cartItem.delete({
+    where: {
+      id: parseInt(req.params.id)
+    }
+  })
+}
+export const changeItemQuantity = async (req: Request, res: Response) => {
+  const validatedData = CartItemSchema.parse(req.body);
+  const userReq = req as unknown as UserParams;
+  const cartItem = await prismaClient.cartItem.findFirst({
+    where: {
+      id: parseInt(req.params.id),
+      userId: userReq.user.id
+    }
+  })
+  if(!cartItem){
+    throw new NotFoundException("Cart item not found", ErrorCode.CART_ITEM_NOT_FOUND);
+  }
+  const updatedCart = await prismaClient.cartItem.update({
+    where: {
+      id: parseInt(req.params.id)
+    },
+    data: {
+      quantity: validatedData.quantity
+    }
+  })
+  res.json(updatedCart);
+}
+export const getCart = async (req: Request, res: Response) => {
+  const cart = await prismaClient.cartItem.findMany({
+    where: {
+      userId: (req as unknown as UserParams).user.id
+    },
+    include: {
+      product: true
+    }
+  })
+  res.json(cart);
+}
