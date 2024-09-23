abb98e262b0a8532f05db9b2f1d70a37f320c63d
belaymit <2belamit@gmail.com>
2024-09-23 12:14:52 +0300

    Added routes for carts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRouter from "./auth";
 import productsRoutes from "./products";
 import usersRouter from "./users";
+import cartRoutes from "./carts";
 
 const rootRouter: Router = Router();
 
 rootRouter.use("/auth", authRouter);
 rootRouter.use("/products", productsRoutes);
 rootRouter.use("/users", usersRouter);
+rootRouter.use("/carts", cartRoutes);
 
 export default rootRouter;
